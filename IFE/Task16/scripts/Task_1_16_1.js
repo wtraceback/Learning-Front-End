051b08c1e5b60b3b6a5a4729e5df72a24cdbf5f7
whxcer <whxcer@outlook.com>
2019-01-20 22:01:00 +0800

    Improve the code of Task16

--- IFE/Task16/scripts/Task_1_16_1.js
+++ IFE/Task16/scripts/Task_1_16_1.js
@@ -1,87 +1,80 @@
-var $ = function(selector) {
-    return document.querySelector(selector);
-}
+(function() {
+    var aqiData = {};
 
-/**
-* aqiData，存储用户输入的空气指数数据
-* 示例格式：
-* aqiData = {
-*    "北京": 90,
-*    "上海": 40
-* };
-*/
-var aqiData = {};
-
-/**
-* 从用户输入中获取数据，向aqiData中增加一条数据
-* 然后渲染aqi-list列表，增加新增的数据
-*/
-function addAqiData() {
-    var pattern = /^[\u4e00-\u9fa5a-zA-Z]+$/;
-    var city = $("#aqi-city-input").value.replace(/\s/g, "");
-    if (!pattern.test(city)) {
-        alert("城市名称必须为中英文字符哦");
-        return;
+    var $ = function(selector) {
+        return document.querySelector(selector);
     }
 
-    var patt = /^[0-9]+$/;
-    var quality = parseInt($("#aqi-value-input").value.replace(/\s/g, ""));
-    if (!patt.test(quality)) {
-        alert("空气质量指数必须为整数哦");
-        return;
+    // 从用户输入中获取数据，向 aqiData 中增加一条数据
+    var addAqiData = function() {
+        var city = $('#aqi-city-input').value.trim();
+        var quality = $('#aqi-value-input').value.trim();
+
+        var cityPattern = /^[a-zA-Z\u4e00-\u9fa5]+$/;
+        if (!cityPattern.test(city)) {
+            alert('输入的城市名必须为汉字或英文！');
+            return;
+        }
+
+        var qualityPattern = /^-?\d+$/;
+        if (!qualityPattern.test(quality)) {
+            alert('输入的空气质量值必须为整数！');
+            return;
+        }
+
+        // 成功时将数据加入对象中并清空输入框
+        aqiData[city] = quality;
+        $('#aqi-city-input').value = "";
+        $('#aqi-value-input').value = "";
     }
 
-    aqiData[city] = quality;
-}
+    // 渲染 aqi-table 表格
+    var renderAqiList = function() {
+        var table = $('#aqi-table');
+        table.innerHTML = '';
 
-/**
-* 渲染aqi-table表格
-*/
-function renderAqiList() {
-    var table = $("#aqi-table");
-    table.innerHTML = "";
+        if (JSON.stringify(aqiData) !== '{}') {
+            table.insertAdjacentHTML('beforeend', `<tr><td>城市</td><td>空气质量</td><td>操作</td></tr>`);
 
-    if (JSON.stringify(aqiData) !== '{}') {
-        table.insertAdjacentHTML('afterbegin', `<tr><td>城市</td><td>空气质量</td><td>操作</td></tr>`);
-        for (var i in aqiData) {
-            table.insertAdjacentHTML('beforeend', `<tr><td>${i}</td><td>${aqiData[i]}</td><td><button>删除</button></td></tr>`)
+            for (var temp in aqiData) {
+                table.insertAdjacentHTML('beforeend', `<tr><td>${temp}</td><td>${aqiData[temp]}</td><td><button>删除</button></td></tr>`)
+            }
         }
     }
-}
 
-/**
-* 点击add-btn时的处理逻辑
-* 获取用户输入，更新数据，并进行页面呈现的更新
-*/
-function addBtnHandle() {
-    addAqiData();
-    renderAqiList();
-}
+    /**
+     * 点击 add-btn 时的处理逻辑
+     * 获取用户输入，更新数据，并进行页面呈现的更新
+     */
+    var addBtnHandle = function() {
+        addAqiData();
+        renderAqiList();
+    }
 
-/**
-* 点击各个删除按钮的时候的处理逻辑
-* 获取哪个城市数据被删，删除数据，更新表格显示
-*/
-function delBtnHandle(event) {
-    var self = event.target;
-
-    if (self.nodeName == "BUTTON" && self.innerText == "删除") {
-        self.parentNode.parentNode.remove();
-
-        var value = self.parentNode.parentNode.children[0].innerText;
-        delete aqiData[value];
+    /**
+     * 点击各个删除按钮的时候的处理逻辑
+     * 获取哪个城市数据被删，删除数据，更新表格显示
+     */
+    var delBtnHandle = function(self) {
+        var city = self.parentNode.parentNode.children[0].innerText;
+        delete aqiData[city];
 
         renderAqiList();
     }
-}
 
-function init() {
+    var __main = function() {
+        $('#add-btn').addEventListener('click', function() {
+            addBtnHandle();
+        });
 
-    // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
-    $("#add-btn").onclick = addBtnHandle;
+        $('#aqi-table').addEventListener('click', function(e) {
+            if (e.target.nodeName == "BUTTON" && e.target.innerText == '删除') {
+                delBtnHandle(e.target);
+            }
+        });
+    }
 
-    // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
-    $("#aqi-table").onclick = delBtnHandle;
-}
+    __main();
 
-init();
+    // 未将数据存入 localStorage 中，每次刷新都得重新添加
+})();
