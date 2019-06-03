c70327c9aa696a9d6b360114a1d1fb458d29ea4b
whxcer <whxcer@outlook.com>
2019-06-03 23:57:37 +0800

    modify js code

--- ToDoList2/scripts/ToDoList.js
+++ ToDoList2/scripts/ToDoList.js
@@ -3,21 +3,29 @@
         return document.querySelector(selector);
     }
 
+    // localStorage.todolist = '[{task: "task", time: "time", done: ture/false}]'
     var todo_list = [];
+    var todo_num = 0;
+    var done_num = 0;
 
     var saveTodoList = function() {
         var result = JSON.stringify(todo_list);
 
-        localStorage.todo = result;
+        localStorage.todolist = result;
     }
 
     var loadTodoList = function() {
-        var str = localStorage.todo;
+        var str = localStorage.todolist;
+
+        var err = [undefined, null];
+        if (err.includes(str)) {
+            return [];
+        }
 
         return JSON.parse(str);
     }
 
-    var listItemTemplate = function(matter) {
+    var listItemTemplate = function(matter, time) {
         var t = `<div class="todo-item">
             <button class="done">Done</button>
             <p class="todo-item-text">${matter}</p>
@@ -27,6 +35,17 @@
         return t;
     }
 
+    var indexOfElement = function(element) {
+        var item = element.parentElement;
+        var children_list = item.parentElement.children;
+
+        for (var i = 0; i < children_list.length; i++) {
+            if (item === children_list[i]) {
+                return i;
+            }
+        }
+    }
+
     var deleteToDoItem = function(element) {
         var index = indexOfElement(element);
         todo_list.splice(index, 1);
@@ -34,24 +53,56 @@
         element.parentElement.remove();
     }
 
-    var insertToDoItem = function(value) {
-        var item = listItemTemplate(value);
+    var insertToDoItem = function(value, time) {
+        var item = listItemTemplate(value, time);
 
         $('.todolist').insertAdjacentHTML('beforeend', item);
-        $('#id-todo-input').value = '';
-        $('#id-todo-input').focus();
     }
 
-    var indexOfElement = function(element) {
-        var item = element.parentElement;
-        var children_list = item.parentElement.children;
+    var insertDoneItem = function(value, time) {
+        var item = listItemTemplate(value, time);
 
-        for (var i = 0; i < children_list.length; i++) {
-            console.log('for')
-            if (item === children_list[i]) {
-                return i;
+        $('.donelist').insertAdjacentHTML('beforeend', item);
+    }
+
+    var stringOfDateTime = function() {
+        var date = new Date();
+
+        var y = date.getFullYear();
+        var m = date.getMonth() + 1;
+        var d = date.getDate();
+        var h = date.getHours();
+        var min = date.getMinutes();
+        var sec = date.getSeconds();
+
+        m = m < 10 ? '0' + m : m;
+        d = d < 10 ? '0' + d : d;
+        h = h < 10 ? '0' + h : h;
+        min = min < 10 ? '0' + min : min;
+        sec = sec < 10 ? '0' + sec : sec;
+
+        return `${y}-${m}-${d} ${h}:${min}:${sec}`;
+    }
+
+    var renderToDoItem = function() {
+        $('.todolist').innerHTML = '';
+        $('.donelist').innerHTML = '';
+        [todo_num, done_num] = [0, 0];
+        for (var i = 0; i < todo_list.length; i++) {
+            var temp = todo_list[i];
+            if (temp.done === true) {
+                insertDoneItem(temp.task, temp.time);
+                ++done_num;
+            } else {
+                insertToDoItem(temp.task, temp.time);
+                ++todo_num;
             }
         }
+
+        $('.todo-num').innerText = todo_num;
+        $('.done-num').innerText = done_num;
+        $('#id-todo-input').value = '';
+        $('#id-todo-input').focus();
     }
 
     var bindEvents = function() {
@@ -64,40 +115,78 @@
                 $('#id-todo-input').focus()
             }
 
-            insertToDoItem(value);
-            todo_list.push(value);
+            for (var i = 0; i < todo_list.length; i++) {
+                if(value === todo_list[i].task) {
+                    alert('任务已存在！！');
+                    return;
+                }
+            }
+
+            var temp = {
+                task: value,
+                time: stringOfDateTime(),
+                done: false,
+            }
+
+            todo_list.push(temp);
+            renderToDoItem();
             saveTodoList();
         });
 
-        // todolist 的事件委托
+        // todo 的事件委托
         $('.todolist').addEventListener('click', function(event) {
-            console.log(event)
-            console.log(event.target.classList)
+            // 删除按钮
             if (event.target.classList.contains('delete')) {
+                --todo_num;
                 deleteToDoItem(event.target);
+                $('.todo-num').innerText = todo_num;
             }
 
+            // 完成按钮
             if (event.target.classList.contains('done')) {
                 var index = indexOfElement(event.target);
-                // TODO
+                todo_list[index].done = true;
+                --todo_num;
+                ++done_num;
+                renderToDoItem();
+                saveTodoList();
+            }
+        })
+
+        // done 的事件委托
+        $('.donelist').addEventListener('click', function(event) {
+            // 删除按钮
+            if (event.target.classList.contains('delete')) {
+                deleteToDoItem(event.target);
+                --done_num;
+                $('.done-num').innerText = done_num;
+            }
+
+            // 完成按钮
+            if (event.target.classList.contains('done')) {
+                var index = indexOfElement(event.target);
+                todo_list[index].done = false;
+                --done_num;
+                ++todo_num;
+                renderToDoItem();
+                saveTodoList();
             }
         })
 
         // clear 清除所有 todolist
         $('.clear').addEventListener('click', function() {
             localStorage.removeItem('todolist');
+            todo_list = [];
+            renderToDoItem();
         });
     }
 
     var __main = function() {
         bindEvents();
 
+        // 将缓存中的 todolist 加载出来
         todo_list = loadTodoList();
-        for (var i = 0; i < todo_list.length; i++) {
-            insertToDoItem(todo_list[i]);
-        }
-
-        $('#id-todo-input').focus();
+        renderToDoItem();
     }
 
     __main();
