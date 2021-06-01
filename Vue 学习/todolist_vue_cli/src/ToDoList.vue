26aa5651495cb9f4265d813983758201edfb351f
wtraceback <whxcer@outlook.com>
2021-06-02 07:31:17 +0800

    Vue 的使用： Vue 项目中 ToDoList 的提交功能和展示功能

---
+++ Vue 学习/todolist_vue_cli/src/ToDoList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+      <div>
+        <input v-model="inputValue" />
+        <button @click="handleSubmit">提交</button>
+      </div>
+      <ul>
+        <li
+            v-for="(item, index) in list"
+            :key="index"
+        >
+            {{ item }}
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            inputValue: '',
+            list: [],
+        }
+    },
+    methods: {
+        handleSubmit: function() {
+            // this.$data.list == this.list
+            this.list.push(this.inputValue)
+            this.inputValue = ''
+        }
+    },
+}
+</script>
+
+<style>
+</style>
