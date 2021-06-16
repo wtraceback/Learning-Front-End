7092f529c502c4a63e6c573dacb8e86c68141943
wtraceback <whxcer@outlook.com>
2021-06-16 23:31:22 +0800

    react-redux 的准备

---
+++ React 学习/todolist_react-redux/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        Hello World
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
