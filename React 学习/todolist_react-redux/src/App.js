75ddc4ec9304a78cbce08fe1f41e1df480f5c517
wtraceback <whxcer@outlook.com>
2021-06-16 23:58:59 +0800

    react-redux 的准备：redux 的使用

--- React 学习/todolist_react-redux/src/App.js
+++ React 学习/todolist_react-redux/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import store from './store/index'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = store.getState()
+  }
+
   render() {
     return (
       <React.Fragment>
-        Hello World
+        <div>
+          <input value={this.state.inputValue} />
+          <button>提交</button>
+        </div>
+        <ul>
+          {
+            this.state.list.map((item, index) => {
+              return (
+                <li key={index}>{item}</li>
+              )
+            })
+          }
+        </ul>
       </React.Fragment>
     );
   }
---
+++ React 学习/todolist_react-redux/src/store/reducer.js
@@ -0,0 +1,8 @@
+const defaultState = {
+    inputValue: '1',
+    list: ['1'],
+}
+
+export default (state=defaultState, action) => {
+    return state
+}
