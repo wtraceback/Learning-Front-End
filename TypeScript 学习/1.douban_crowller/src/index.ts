5bb5fd11d6501c57e1f0b18b1d13dc23ad068e4b
wtraceback <whxcer@outlook.com>
2021-08-24 23:03:16 +0800

    尝试使用 superagent 获取页面的 html

--- TypeScript 学习/1.douban_crowller/src/index.ts
+++ TypeScript 学习/1.douban_crowller/src/index.ts
@@ -1,1 +1,19 @@
-console.log('hello world');
+import superagent from 'superagent'
+
+class Crowller {
+    constructor(url: string) {
+        this.getHtml(url)
+    }
+
+    async getHtml(url: string) {
+        var result = await superagent.get(url)
+        console.log(result.text);
+    }
+}
+
+var main = function() {
+    var url = 'https://movie.douban.com/top250'
+    const crowller = new Crowller(url)
+}
+
+main()
