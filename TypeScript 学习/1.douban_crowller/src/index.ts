81dbee829cb8904826e11be21008a2e04e8c9d21
wtraceback <whxcer@outlook.com>
2021-08-30 07:54:40 +0800

    使用 cheerio 来解析爬取到的 豆瓣电影Top250 的网页信息

--- TypeScript 学习/1.douban_crowller/src/index.ts
+++ TypeScript 学习/1.douban_crowller/src/index.ts
@@ -1,19 +1,47 @@
 import superagent from 'superagent'
+import cherrio from 'cheerio'
 
-class Crowller {
-    constructor(url: string) {
-        this.getHtml(url)
+class Movie {
+    public name: string
+    public score: string
+    public quote: string
+    public cover_url: string | undefined
+    public ranking: string
+
+    constructor() {
+        this.name = ''
+        this.score = ''
+        this.quote = ''
+        this.cover_url = ''
+        this.ranking = ''
     }
 
-    async getHtml(url: string) {
-        var result = await superagent.get(url)
-        console.log(result.text);
+    toString() {
+
     }
+}
+
+var movies_from_url = async function(url: string) {
+    var r = await superagent.get(url)
+    var page = r.text
+    var $ = cherrio.load(page)
+    var items = $('.item')
+    var movies = []
+    items.map((index, e) => {
+        var m = new Movie()
+        m.name = $(e).find('.title').text()
+        m.score = $(e).find('.rating_num').text()
+        m.quote = $(e).find('.inq').text()
+        m.cover_url = $(e).find('img').attr('src')
+        m.ranking = $(e).find('.pic').find('em').text()
+        movies.push(m)
+        console.log(m);
+    })
 }
 
 var main = function() {
     var url = 'https://movie.douban.com/top250'
-    const crowller = new Crowller(url)
+    const movies = movies_from_url(url)
 }
 
 main()
