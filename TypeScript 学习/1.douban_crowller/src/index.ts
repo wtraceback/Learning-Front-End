d017e5556c33afb7cfa9aa535d18f92e3f0530b2
wtraceback <whxcer@outlook.com>
2021-09-04 10:29:58 +0800

    使用 fs 和 path 模块读写文件

--- TypeScript 学习/1.douban_crowller/src/index.ts
+++ TypeScript 学习/1.douban_crowller/src/index.ts
@@ -1,5 +1,7 @@
 import superagent from 'superagent'
 import cherrio from 'cheerio'
+import fs from 'fs'
+import path from 'path'
 
 class Movie {
     public name: string
@@ -21,12 +23,26 @@ class Movie {
     }
 }
 
+var save_data = function(data: Movie[]) {
+    var filepath = path.resolve(__dirname, '../data/douban_top250.json')
+    var file_content: Movie[] = []
+    if (fs.existsSync(filepath)) {
+        file_content = JSON.parse(fs.readFileSync(filepath, 'utf-8'))
+        console.log('file_content', file_content);
+    }
+
+    file_content = file_content.concat(data)
+    fs.writeFileSync(filepath, JSON.stringify(file_content))
+}
+
 var movies_from_url = async function(url: string) {
+    console.log('before superagent.get');
     var r = await superagent.get(url)
+    console.log('after superagent.get');
     var page = r.text
     var $ = cherrio.load(page)
     var items = $('.item')
-    var movies = []
+    var movies: Movie[] = []
     items.map((index, e) => {
         var m = new Movie()
         m.name = $(e).find('.title').text()
@@ -35,13 +51,19 @@ var movies_from_url = async function(url: string) {
         m.cover_url = $(e).find('img').attr('src')
         m.ranking = $(e).find('.pic').find('em').text()
         movies.push(m)
-        console.log(m);
     })
+
+    return movies
 }
 
 var main = function() {
     var url = 'https://movie.douban.com/top250'
-    const movies = movies_from_url(url)
+    console.log('before movies_from_url');
+    movies_from_url(url).then((res) => {
+        save_data(res)
+        console.log('save success');
+    })
+    console.log('after movies_from_url');
 }
 
 main()
