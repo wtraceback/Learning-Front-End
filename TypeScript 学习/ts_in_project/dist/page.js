aa652558d6ccb23844300148e62b4939586e8163
wtraceback <whxcer@outlook.com>
2021-09-18 07:39:15 +0800

    使用类组件的形式创建 html 元素

--- TypeScript 学习/ts_in_project/dist/page.js
+++ TypeScript 学习/ts_in_project/dist/page.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log('hello world');
+var Header = /** @class */ (function () {
+    function Header() {
+        var e = "\n            <div>Header</div>\n        ";
+        document.body.insertAdjacentHTML('afterbegin', e);
+    }
+    return Header;
+}());
+var Content = /** @class */ (function () {
+    function Content() {
+        var e = "\n            <div>Content</div>\n        ";
+        document.body.insertAdjacentHTML('afterbegin', e);
+    }
+    return Content;
+}());
+var Footer = /** @class */ (function () {
+    function Footer() {
+        var e = "\n            <div>Footer</div>\n        ";
+        document.body.insertAdjacentHTML('afterbegin', e);
+    }
+    return Footer;
+}());
+var Page = /** @class */ (function () {
+    function Page() {
+        new Footer();
+        new Content();
+        new Header();
+    }
+    return Page;
+}());
--- TypeScript 学习/ts_in_project/src/page.ts
+++ TypeScript 学习/ts_in_project/src/page.ts
@@ -1,1 +1,34 @@
-console.log('hello world')
+class Header {
+    constructor() {
+        const e = `
+            <div>Header</div>
+        `
+        document.body.insertAdjacentHTML('afterbegin', e)
+    }
+}
+
+class Content {
+    constructor() {
+        const e = `
+            <div>Content</div>
+        `
+        document.body.insertAdjacentHTML('afterbegin', e)
+    }
+}
+
+class Footer {
+    constructor() {
+        const e = `
+            <div>Footer</div>
+        `
+        document.body.insertAdjacentHTML('afterbegin', e)
+    }
+}
+
+class Page {
+    constructor() {
+        new Footer()
+        new Content()
+        new Header()
+    }
+}
