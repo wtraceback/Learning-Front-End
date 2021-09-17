060d3819f3bc8bd7c6834ee344fc79d12b6f2708
wtraceback <whxcer@outlook.com>
2021-09-18 07:55:19 +0800

    使用命名空间 namespace 限制全局函数

--- TypeScript 学习/ts_in_project/dist/page.js
+++ TypeScript 学习/ts_in_project/dist/page.js
@@ -1,30 +1,34 @@
 "use strict";
-var Header = /** @class */ (function () {
-    function Header() {
-        var e = "\n            <div>Header</div>\n        ";
-        document.body.insertAdjacentHTML('afterbegin', e);
-    }
-    return Header;
-}());
-var Content = /** @class */ (function () {
-    function Content() {
-        var e = "\n            <div>Content</div>\n        ";
-        document.body.insertAdjacentHTML('afterbegin', e);
-    }
-    return Content;
-}());
-var Footer = /** @class */ (function () {
-    function Footer() {
-        var e = "\n            <div>Footer</div>\n        ";
-        document.body.insertAdjacentHTML('afterbegin', e);
-    }
-    return Footer;
-}());
-var Page = /** @class */ (function () {
-    function Page() {
-        new Footer();
-        new Content();
-        new Header();
-    }
-    return Page;
-}());
+var Home;
+(function (Home) {
+    var Header = /** @class */ (function () {
+        function Header() {
+            var e = "\n                <div>Header</div>\n            ";
+            document.body.insertAdjacentHTML('afterbegin', e);
+        }
+        return Header;
+    }());
+    var Content = /** @class */ (function () {
+        function Content() {
+            var e = "\n                <div>Content</div>\n            ";
+            document.body.insertAdjacentHTML('afterbegin', e);
+        }
+        return Content;
+    }());
+    var Footer = /** @class */ (function () {
+        function Footer() {
+            var e = "\n                <div>Footer</div>\n            ";
+            document.body.insertAdjacentHTML('afterbegin', e);
+        }
+        return Footer;
+    }());
+    var Page = /** @class */ (function () {
+        function Page() {
+            new Footer();
+            new Content();
+            new Header();
+        }
+        return Page;
+    }());
+    Home.Page = Page;
+})(Home || (Home = {}));
--- TypeScript 学习/ts_in_project/src/page.ts
+++ TypeScript 学习/ts_in_project/src/page.ts
@@ -1,34 +1,36 @@
-class Header {
-    constructor() {
-        const e = `
-            <div>Header</div>
-        `
-        document.body.insertAdjacentHTML('afterbegin', e)
+namespace Home {
+    class Header {
+        constructor() {
+            const e = `
+                <div>Header</div>
+            `
+            document.body.insertAdjacentHTML('afterbegin', e)
+        }
     }
-}
 
-class Content {
-    constructor() {
-        const e = `
-            <div>Content</div>
-        `
-        document.body.insertAdjacentHTML('afterbegin', e)
+    class Content {
+        constructor() {
+            const e = `
+                <div>Content</div>
+            `
+            document.body.insertAdjacentHTML('afterbegin', e)
+        }
     }
-}
 
-class Footer {
-    constructor() {
-        const e = `
-            <div>Footer</div>
-        `
-        document.body.insertAdjacentHTML('afterbegin', e)
+    class Footer {
+        constructor() {
+            const e = `
+                <div>Footer</div>
+            `
+            document.body.insertAdjacentHTML('afterbegin', e)
+        }
     }
-}
 
-class Page {
-    constructor() {
-        new Footer()
-        new Content()
-        new Header()
+    export class Page {
+        constructor() {
+            new Footer()
+            new Content()
+            new Header()
+        }
     }
 }
