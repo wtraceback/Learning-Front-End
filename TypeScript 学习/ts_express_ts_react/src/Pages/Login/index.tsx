a936aad088fe6bb273cac75a8b0a55c2f3d51b7c
wtraceback <whxcer@outlook.com>
2021-09-26 23:30:52 +0800

    登录页逻辑的编写

--- TypeScript 学习/ts_express_ts_react/src/Pages/Login/index.tsx
+++ TypeScript 学习/ts_express_ts_react/src/Pages/Login/index.tsx
@@ -1,57 +1,89 @@
-import { Form, Input, Button } from "antd";
+import React, { Component } from 'react'
+import { Form, Input, Button, message } from "antd";
+import { Redirect } from "react-router-dom";
+import qs from 'qs'
+import axios from 'axios'
 import styles from "./index.module.css";
 
-const Login = () => {
-    const onFinish = (values: any) => {
+class Login extends Component {
+    state = {
+        isLogin: false,
+    }
+
+    onFinish = (values: any) => {
         console.log("Success:", values);
+        // axios.post("/api/login", qs.stringify({
+        axios.post("/login", qs.stringify({
+            username: values.username,
+            password: values.password,
+        }),
+        {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        }).then((res) => {
+            if (res.data.success) {
+                this.setState({
+                    isLogin: true
+                })
+            } else {
+                message.error(res.data.msg);
+            }
+        });
     };
 
-    const onFinishFailed = (errorInfo: any) => {
+    onFinishFailed = (errorInfo: any) => {
         console.log("Failed:", errorInfo);
     };
 
-    return (
-        <div className={styles.main}>
-            <Form
-                name="basic"
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <Form.Item
-                    label="username"
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: "请输入用户名",
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
+    render() {
+        if (this.state.isLogin) {
+            return <Redirect to="/"></Redirect>
+        } else {
+            return (
+                <div className={styles.main}>
+                    <Form
+                        name="basic"
+                        onFinish={this.onFinish}
+                        onFinishFailed={this.onFinishFailed}
+                        autoComplete="off"
+                    >
+                        <Form.Item
+                            label="username"
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "请输入用户名",
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
 
-                <Form.Item
-                    label="password"
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: "请输入登录密码",
-                        },
-                    ]}
-                >
-                    <Input.Password />
-                </Form.Item>
+                        <Form.Item
+                            label="password"
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: "请输入登录密码",
+                                },
+                            ]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
 
-                <Form.Item>
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
-        </div>
-    );
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+            );
+        }
+    }
 };
 
 export default Login;
