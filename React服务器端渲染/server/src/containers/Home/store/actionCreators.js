7e06e11bf8be3ae0cc33df91c81e35c68ff72656
wtraceback <whxcer@outlook.com>
2021-10-05 16:00:55 +0800

    优化代码：redux-thunk 中 withExtraArgument 的使用

--- React服务器端渲染/server/src/containers/Home/store/actionCreators.js
+++ React服务器端渲染/server/src/containers/Home/store/actionCreators.js
@@ -1,12 +1,8 @@
-import { instance as clientAxios } from '../../../client/request'
-import { instance as serverAxios } from '../../../server/request'
 import * as actionTypes from './actionTypes'
 
-export const initData = (server) => {
-    let request = server ? serverAxios : clientAxios
-
-    return (dispatch) => {
-        return request.get('/api/books')
+export const initData = () => {
+    return (dispatch, getState, axiosInstance) => {
+        return axiosInstance.get('/api/books')
             .then((res) => {
                 dispatch(initDataAction(res.data))
             })
--- React服务器端渲染/server/src/store/index.js
+++ React服务器端渲染/server/src/store/index.js
@@ -1,16 +1,20 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { reducer as homeReducer } from "../containers/Home/store";
+import { instance as clientAxios } from '../client/request'
+import { instance as serverAxios } from '../server/request'
 
 const reducer = combineReducers({
     home: homeReducer,
 });
 
 export const getStore = () => {
-    return createStore(reducer, applyMiddleware(thunk));
+    // 在 actionCreators 里面，store.dispatch 使用的是对应的服务器或者是客户端的 store
+    // 因此可以使用 redux-thunk 中间件额外传入的参数
+    return createStore(reducer, applyMiddleware(thunk.withExtraArgument(serverAxios)));
 };
 
 export const getClientStore = () => {
     const defaultState = window.context.state
-    return createStore(reducer, defaultState, applyMiddleware(thunk));
+    return createStore(reducer, defaultState, applyMiddleware(thunk.withExtraArgument(clientAxios)));
 }
