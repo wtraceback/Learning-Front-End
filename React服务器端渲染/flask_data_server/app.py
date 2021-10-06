91320d9af63e305442b6295e97e3cc09f4d707a9
wtraceback <whxcer@outlook.com>
2021-10-06 11:16:03 +0800

    API 服务器登录和未登录获取的数据的区分

--- React服务器端渲染/flask_data_server/app.py
+++ React服务器端渲染/flask_data_server/app.py
@@ -9,21 +9,15 @@ login_status = {
 }
 
 
-@app.route("/ssr/api/books", methods=["GET"])
+@app.route("/ssr/api/book_title", methods=["GET"])
 @cross_origin()
 def getData():
     books = [
         {
-            "id": 1,
-            "title": u"论语",
-            "author": u"孔子",
-            "price": 18
-        },
-        {
-            "id": 2,
-            "title": u"道德经",
-            "author": u"老子",
-            "price": 15
+            "id": 0,
+            "title": u"书籍名",
+            "author": u"作者",
+            "price": u"价格",
         }
     ]
 
@@ -50,3 +44,27 @@ def Logout():
     login_status["login"] = False
 
     return jsonify(responseData(True, "", login_status))
+
+
+@app.route('/ssr/api/book_info', methods=["GET"])
+@cross_origin()
+def translation():
+    books = [
+        {
+            "id": 1,
+            "title": u"论语",
+            "author": u"孔子",
+            "price": 18
+        },
+        {
+            "id": 2,
+            "title": u"道德经",
+            "author": u"老子",
+            "price": 15
+        }
+    ]
+
+    if login_status["login"]:
+        return jsonify(responseData(True, "", books))
+    else:
+        return jsonify(responseData(False, "", {}))
